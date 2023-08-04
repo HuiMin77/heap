0121df775dbbcaa48321d5227a57f97908dd6481
Poh Hui Min <huimin.poh.2021@scis.smu.edu.sg>
2023-08-04 14:43:43 +0800

    modified user registration form

--- ExcoVerse/ExcoVerse_Website/members/forms.py
+++ ExcoVerse/ExcoVerse_Website/members/forms.py
@@ -4,15 +4,16 @@ from django.contrib.auth.models import User
 from django import forms
 from events.models import CCA
 class RegisterUserForm(UserCreationForm):
-    email = forms.EmailField(widget=forms.EmailInput(attrs={'class':'form-control'}))
-    first_name = forms.CharField(max_length=50,widget=forms.TextInput(attrs={'class':'form-control'}))
-    last_name = forms.CharField(max_length=50,widget=forms.TextInput(attrs={'class':'form-control'}))
-    student_id = forms.CharField(max_length=50, widget=forms.TextInput(attrs={'class': 'form-control'}))
-    mobile_number = forms.CharField(max_length=50, widget=forms.TextInput(attrs={'class': 'form-control'}))
+    # email = forms.EmailField(widget=forms.EmailInput(attrs={'class':'form-control'}))
+    # first_name = forms.CharField(max_length=50,widget=forms.TextInput(attrs={'class':'form-control'}))
+    # last_name = forms.CharField(max_length=50,widget=forms.TextInput(attrs={'class':'form-control'}))
+    # student_id = forms.CharField(max_length=50, widget=forms.TextInput(attrs={'class': 'form-control'}))
+    # mobile_number = forms.CharField(max_length=50, widget=forms.TextInput(attrs={'class': 'form-control'}))
     username = forms.ModelChoiceField(queryset=CCA.objects.all(), widget=forms.Select(attrs={'class': 'form-control'}))
     class Meta:
         model = User
-        fields = ('first_name','last_name','email','student_id','mobile_number','username','password1','password2')
+        # 'first_name','last_name','email','student_id','mobile_number',
+        fields = ('username','password1','password2')
     
     def __init__(self, *args, **kwargs):
         super(RegisterUserForm, self).__init__(*args, **kwargs)
